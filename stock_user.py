97e8f4fa94cb81b45e0405ab19e0cf256bf06f58
atlomak <01159465@pw.edu.pl>
2021-10-16 19:00:08 +0200

    Data about user dodane

---
+++ stock_user.py
@@ -0,0 +1,21 @@
+import http.client
+from base64 import b64encode
+import json
+
+class User:
+    def __init__(self, mail, password) -> None:
+        self.mail = mail                #ustawienie maila dla tej instacji
+        self.password = password        #ustawienie hasla
+        self.header = {
+            "Authorization":"Basic {}".format(
+            b64encode(bytes(f"{self.mail}:{self.password}", "utf-8")).decode("ascii"))
+            }
+        self.conn = http.client.HTTPSConnection("zsutstockserver.azurewebsites.net")     #polaczenie z autoryzacja przez url
+    def data(self):
+        self.conn.request("GET", "/api/client", headers=self.header)
+        data = self.conn.getresponse().read()
+        data = json.loads(data)
+        print(data)
+
+andrzej = User("01159465@pw.edu.pl","1Lab1")
+andrzej.data()
