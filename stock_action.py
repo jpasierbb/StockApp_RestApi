8dafd664cb637b658ecb05ec259062a93a611b36
atlomak <01159465@pw.edu.pl>
2021-10-16 23:39:35 +0200

    dzialajacy modul zakupu/sprzedazy akcji

--- stock_action.py
+++ stock_action.py
@@ -2,34 +2,50 @@ import http.client
 import json
 from base64 import b64encode
 from stock_checking import stockData
+import math
 
 class stockDo:
 
     def __init__(self, login, password) -> None:
         self.login = str(login)
         self.password = str(password)
+        self.header = {
+            "Authorization": "Basic {}".format(
+                b64encode(bytes(f"{self.login}:{self.password}", "utf-8")).decode("ascii")),
+            "Content-Type": "application/json"
+        }
         self.conn = http.client.HTTPSConnection("zsutstockserver.azurewebsites.net")
-
-    def stock_action(self, stock, name, buySell, amount):
+    def stock_action(self, exchange, share, buySell, amount):
         # Klasa zczytuje ceny i ilosc akcji sprzedazy/kupowania
         # Cena danej spolki
         # Limit czyli ilosc dostepnych akcji, przyda sie zeby program nie chcial kupic wiecej niz jest
         info = stockData()
-        price_limit = info.check_price(stock, name, buySell)
-        price = price_limit[0]
-        limit = price_limit[1]
-
-        #paramtery, ktore trzerba podac do kupna sprzedazy, login itd
-        header = {
-            "Authorization": "Basic {}".format(
-                b64encode(bytes(f"{self.login}:{self.password}", "utf-8")).decode("ascii")),
-        }
-        body = json.dumps({"price": price, "buySell": str(buySell), "amount": amount,
-            "stockExchange": str(stock), "share": str(name)})
+        if buySell == "buy":    #dla kupienia trzeba sprawdzic cene sprzedazy i na odwrot
+            price = (info.check_price(exchange, share, "sell"))[0]
+            price = math.ceil(price)
+            print(price)    #zaokraglenie do calkowitych od gory
+            limit = (info.check_price(exchange, share, "sell"))[1]
+            if limit < amount:      #jezeli wiecej masz kupic niz mozna ustawia sie na max dostepnych
+                amount = limit
+        elif buySell == "sell":
+            price = (info.check_price(exchange, share, "buy"))[0]
+            price = math.floor(price)   #zaokraglenie do calkowitych od dolu
+            limit = (info.check_price(exchange, share, "buy"))[1]
+            if limit < amount:
+                amount = limit
+        body = json.dumps({
+                "stockExchange":f'{exchange}',
+                "share":f'{share}',
+                "amount":amount,
+                "price":price
+                })
 
         #komenda zakupu
-        self.conn.request("POST", url=f"/api/{buySell}offer", headers=header, body=body)
-        response = self.conn.getresponse()
+        self.conn.request("POST", url=f"/api/{buySell}offer", headers=self.header, body=body)   #wysyla zlecenie
+        if self.conn.getresponse().status == 200:
+            return "ok"
+        else:
+            print(self.conn.getresponse().read())
 
         #print(response)
         #print(response.status)
@@ -39,6 +55,10 @@ class stockDo:
 
 
 if __name__ == "__main__":
-    action = stockDo("01159465@pw.edu.pl","1Lab1")
-    action.stock_action("Warszawa", "11BIT", "buy", 10)
-    print(action.stock_action("Warszawa", "11BIT", "buy", 10))
+    mail = input("podaj mail ")
+    password = input("podaj haslo ")
+    gielda = input("Podaj w tej kolejnosci po spacji: gielda nazwa_akcji buy_or_sell ilosc: ")
+    gielda = gielda.split()
+    action = stockDo(mail, password)
+    action.stock_action(gielda[0],gielda[1],gielda[2],int(gielda[3]))
+    
--- stock_checking.py
+++ stock_checking.py
@@ -5,7 +5,8 @@ class stockData:
         self.conn = http.client.HTTPSConnection("zsutstockserver.azurewebsites.net")        #nawiazanie polaczenia(jednorazowo przy wywolaniu instancji)
     def check_price(self, exchange, shares, buySell):                                        #sprawdzanie cen akcji, wymagana nazwa gieldy, nazwa akcji
         self.conn.request("GET", url=f"/api/shareprice/{exchange}?share={shares}")           #request
-        data = self.conn.getresponse().read()           
+        data = self.conn.getresponse().read()
+        self.conn.close()           
         data = data.decode("utf-8") 
         data = json.loads(data)         #zamiana na liste tablic
         buy = data[0]                   #pierwszy dict zawiera ceny buy
@@ -17,12 +18,14 @@ class stockData:
     def check_exchanges(self):                                                               #sprawdzanie dostepnych gield
         self.conn.request("GET", url=f"/api/stockexchanges")
         data = self.conn.getresponse().read()
+        self.conn.close()
         data = data.decode("utf-8")
         data = json.loads(data)
         return data
     def check_shares(self, exchange):                                                          #sprawdzanie dostepnych akcji, wymagana nazwa gieldy
         self.conn.request("GET", url=f"/api/shareslist/{exchange}")
         data = self.conn.getresponse().read()
+        self.conn.close()
         data = data.decode("utf-8")
         data = json.loads(data)
         return data
