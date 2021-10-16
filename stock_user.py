b998fa7ca314516eddf7e765c462e4b3f24a6b3e
atlomak <01159465@pw.edu.pl>
2021-10-16 19:49:54 +0200

    user info, funds, history

--- stock_user.py
+++ stock_user.py
@@ -8,14 +8,34 @@ class User:
         self.password = password        #ustawienie hasla
         self.header = {
             "Authorization":"Basic {}".format(
-            b64encode(bytes(f"{self.mail}:{self.password}", "utf-8")).decode("ascii"))
+            b64encode(bytes(f"{self.mail}:{self.password}", "utf-8")).decode("ascii"))  #
             }
         self.conn = http.client.HTTPSConnection("zsutstockserver.azurewebsites.net")     #polaczenie z autoryzacja przez url
-    def data(self):
+    def userinfo(self):
         self.conn.request("GET", "/api/client", headers=self.header)
         data = self.conn.getresponse().read()
+        self.conn.close()
         data = json.loads(data)
         print(data)
+    def userfunds(self):
+        self.conn.request("GET", "/api/client", headers=self.header)
+        data = self.conn.getresponse().read()
+        self.conn.close()
+        data = json.loads(data)
+        funds = data["funds"]
+        return funds
+    def history(self):
+        self.conn.request("GET", "/api/history", headers=self.header)
+        data = self.conn.getresponse().read()
+        self.conn.close()
+        data = json.loads(data)
+        return data
 if __name__ == "__main__":
-    andrzej = User("01159465@pw.edu.pl","1Lab1")
-    andrzej.data()
+    mail = input("wprowadz mail ")
+    password = input("wprowadz haslo ")
+    user = User(mail,password)
+    print(user.userinfo())
+    print(f"\n"*3)
+    print(user.history())
+    print(f"\n"*3)
+    print(user.userfunds())
