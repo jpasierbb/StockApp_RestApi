615db38a70b1160c554935bb493f479fa5558e4a
Kuba <jakub.pasierb.stud@pw.edu.pl>
2021-10-17 21:55:24 +0200

    REST 3.3 - 40 spolek, tyle akcji ile liter w nazwie

--- REST_3_3.py
+++ REST_3_3.py
@@ -3,34 +3,63 @@ from stock_action import stockDo
 from stock_user import User
 import random
 
-def REST_3_3(konto):
-    acc = konto
+def REST_3_3(acc):
     #print(type(acc.shares()))
-
-    for i in range(100):
-        exchanges = info.check_exchanges()
-        exchange = exchanges[random.randint(0, len(exchanges) - 1)]  # losowa gielda z przedzialu 0 - liczba gield
-        shares = info.check_shares(exchange)
-        share = shares[random.randint(0, len(shares) - 1)]
-        action.stock_action(exchange, share, "buy", 1)
+    while True:
         amount = 0
         for key, value in acc.shares().items():
             amount += 1
         if amount == 40:
-            print(amount)
+            print("Shares amount = 40 - OK")
             break
+        elif amount < 40:
+            while True:
+                exchanges = info.check_exchanges()
+                exchange = exchanges[random.randint(0, len(exchanges) - 1)]  # losowa gielda z przedzialu 0 - liczba gield
+                shares = info.check_shares(exchange)
+                share = shares[random.randint(0, len(shares) - 1)]
+                action.stock_action(exchange, share, "buy", 1)
+                amount += amount
+                if amount == 40:
+                    break
+        elif amount > 40:
+            shares = []
+            for key, value in acc.shares().items():
+                shares.append(key)
+            share = shares[random.randint(0, len(shares) - 1)]
+            while True:
+                if acc.shares()[share] == 1:
+                    exchange = info.where(share)
+                    action.stock_action(exchange, share, "sell", 1)
+                    break
+                exchange = info.where(share)
+                action.stock_action(exchange, share, "sell", 1)
+
+
+
 
     for key, value in acc.shares().items():
         share = key
+        amount = value
         exchange = info.where(share)
-        if value < len(key):
-            for i in range(20):
-                if value < len(key):
+        print("")
+        print(share, amount)
+        if amount < len(share):
+            while True:
+                if amount < len(share):
                     action.stock_action(exchange, share, "buy", 1)
-        if value > len(key):
-            for i in range(20):
-                if value > len(key):
+                    amount += 1
+                    print(share, amount)
+                if amount == len(share):
+                    break
+        if amount > len(share):
+            while True:
+                if amount > len(share):
                     action.stock_action(exchange, share, "sell", 1)
+                    amount -= 1
+                    print(share, amount)
+                if amount == len(share):
+                    break
 
 
 
