aa39079596f4a2a9efa5eaa34bbb9eab5b84e9f7
Kuba <104521083+jpasierbb@users.noreply.github.com>
2024-10-18 17:09:08 +0200

    Update REST_sell_all_reset.py

--- REST_sell_all_reset.py
+++ REST_sell_all_reset.py
@@ -5,9 +5,9 @@ x = input("Podaj komu wyzerowac akcje: \
     \n 1 - Andrzej \
     \n 2 - Kuba")
 if x == "1":
-    user = User(login="01159465@pw.edu.pl", password="1Lab1")
+    user = User("example_email_one@example.com", "passwordOne")
 elif x == "2":
-    user = User(login="01161816@pw.edu.pl",password="kubapasierb1")
+    user = User("example_email_two@example.com", "passwordTwo")
 else:
     raise Exception("Blad wyboru")
 action = stockDo(user=user)
